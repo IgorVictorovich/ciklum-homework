4a9528e1ab73a359e44c2d7a568d19bd23edabbc
= <=>
2017-05-28 17:51:36 +0300

    added readonly mode, delete utils

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { Credentials, AccountsService } from './app.accounts.service';
-import { Utils } from './app.utils';
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [ AccountsService, Credentials, Utils]
+  providers: [ AccountsService, Credentials]
 })
 export class AppComponent {}
